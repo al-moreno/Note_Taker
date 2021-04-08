85d8404901a8f547dacd47a805c3211392713a62
al-moreno <77991840+al-moreno@users.noreply.github.com>
2021-04-08 15:59:44 -0500

    7th commit
    deleted previous repository with previous 6 commits. Still not working

---
+++ server.js
@@ -0,0 +1,71 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const fs = require('fs');
+
+
+const PORT = process.env.PORT || 8000;
+const path = require('path');
+const app = express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => { res.sendFile(path.join(__dirname, 'Develop/public/index.html')); });
+app.get('/notes', (req, res) => { res.sendFile(path.join(__dirname, 'Develop/public/notes.html')); });
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/api/notes', (req, res) => {
+  fs.readFile('db/db.json', 'utf8', function (err, contents) {
+    let words = JSON.parse(contents);
+    res.send(words);
+  });
+});
+
+app.post('/api/notes', (req, res) => {
+  fs.readFile('db/db.json', (err, data) => {
+
+    if (err) throw err;
+
+    let json = JSON.parse(data);
+    let note = {
+      title: req.body.title,
+      text: req.body.text,
+  
+    }
+    json.push(note);
+
+
+    fs.writeFile('db/db.json', JSON.stringify(json, null, 2), (err) => {
+
+      if (err) throw err;
+      res.send('200');
+    });
+  });
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+
+  fs.readFile('db/db.json', (err, data) => {
+
+    if (err) throw err;
+    let deleteId = req.params.id;
+
+    let json = JSON.parse(data);
+    json.forEach((item, i) => {
+      if (item.id.includes(deleteId)) {
+        json.splice(i, 1);
+      }
+    });
+
+    fs.writeFile('db/db.json', JSON.stringify(json, null, 2), (err) => {
+
+      if (err) throw err;
+      res.send('200');
+    });
+  });
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);});
